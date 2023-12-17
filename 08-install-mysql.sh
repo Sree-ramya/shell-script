b0e975bfced6095512ddf16dfe2b66c73f844ad4
Sree-ramya <ksreeramya830@gmail.com>
2023-12-17 22:27:16 +0530

    eight commit to install sql

--- 08-install-mysql.sh
+++ 08-install-mysql.sh
@@ -5,8 +5,17 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: Please run the script with root access"
+    exit 1
 else
     echo "You are root user"
 fi
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing MYSQL is failed"
+    exit 1
+else
+    echo "Installing MYSQL is success"
+fi    
